3753272375c00aafa393981a4f0fef4cb2cea634
sehdev kumar <kumarsehdev600@gmail.com>
2023-01-16 20:18:48 +0530

    Automatic commit from bash script

---
+++ git-make.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# navigate to the root of the git repository
+cd $(git rev-parse --show-toplevel)
+
+# add all changes to the git index
+git add .
+
+# commit changes with a message
+git commit -m "Automatic commit from bash script"
+
+# push the commit to the remote repository
+git push
